b442f1baeca738f41bd73d55a244ce57016e2de2
newericg1~ <eric.gomes0105@gmail.com>
2024-06-05 12:36:09 -0300

    feat: added base dependecies and started navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './core/interfaces/home-page/home-page.component';
+import { UcListComponent } from './core/interfaces/uc-list/uc-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'uc-list', component: UcListComponent },
+
+];
---
+++ src/app/core/interfaces/uc-list/uc-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uc-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './uc-list.component.html',
+  styleUrl: './uc-list.component.scss'
+})
+export class UcListComponent {
+
+}
