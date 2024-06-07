9662f374afc3dbdc17cdce6adc131694ca7da58a
newericg1~ <eric.gomes0105@gmail.com>
2024-06-07 04:01:25 -0300

    feat: added mobile fixes for main page and top navbar

--- src/app/features/pages/uc-list/uc-list.component.ts
+++ src/app/features/pages/uc-list/uc-list.component.ts
@@ -1,46 +1,142 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import ResponseData from '../../../../../ucs-response.json';
 import { ButtonComponent } from '../../components/button/button.component';
 import { CardComponent } from '../../components/card/card.component';
 import { TableComponent } from '../../components/table/table.component';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-uc-list',
   standalone: true,
-  imports: [ ButtonComponent, CardComponent, TableComponent],
+  imports: [ButtonComponent, CardComponent, TableComponent],
   templateUrl: './uc-list.component.html',
-  styleUrl: './uc-list.component.scss'
+  styleUrl: './uc-list.component.scss',
 })
 export class UcListComponent implements OnInit {
+  constructor(private _route: Router) {}
 
-  constructor(private _route:Router) {}
+  // response = [];
+  response = ResponseData
 
+  tableHeaderList: any = {};
+  totalUCs!: number;
 
-  cardList: { title: string}[] = [
+  cardList: any[] = [
     {
-      title: 'Total UCs'
+      title: 'Total UCs',
+      totalUCs: null,
     },
     {
-      title: 'Status'
+      title: 'Status',
+      ativos: null,
+      inativos: null,
     },
     {
-      title: 'Tipo'
+      title: 'Tipo',
+      consumidor: null,
+      gerador: null,
     },
     {
-      title: 'Grupo'
+      title: 'Grupo',
+      grupoA: null,
+      grupoB: null,
     },
     {
-      title: 'Dados de faturamento'
-    }
-  ]
+      title: 'Dados de faturamento',
+      comEndereco: null,
+      semEndereco: null,
+      comCPF: null,
+      semCPF: null,
+    },
+  ];
 
   ngOnInit(): void {
-    // console.log(this.activatedRoute.snapshot.routeConfig?.title)
+    this.getResponseHeader();
+    this.getCardsDatas();
+    this.getActiveAndInactiveStatus();
+    this.getTipo();
+    this.getGrupo();
+    this.getAddressAndCpfCount();
   }
-  
 
   navigateTo(route: string) {
-    console.log(route)
     this._route.navigate([route]);
+  }
+
+  getResponseHeader() {
+    if(this.response.length) {
+      this.tableHeaderList = Object.keys(this.response[0]);
+    }
+    }
+
+  getCardsDatas() {
+    if (this.response) {
+      this.totalUCs = this.response.length;
+      this.cardList[0].totalUCs = this.totalUCs;
+    }
+  }
+
+  getActiveAndInactiveStatus() {
+    let active: number = 0;
+    let inactive: number = 0;
+    if (this.response) {
+      this.response.map((uc: any) => {
+        uc.Status === false ? (inactive = inactive + 1) : (active = active + 1);
+      });
+      this.cardList[1].ativos = active;
+      this.cardList[1].inativos = inactive;
+    }
+  }
+
+  getTipo() {
+    let consumidor: number = 0;
+    let gerador: number = 0;
+    if (this.response) {
+      this.response.map((uc: any) => {
+        uc.Tipo === 'Consumidor'
+          ? (consumidor = consumidor + 1)
+          : (gerador = gerador + 1);
+      });
+      this.cardList[2].gerador = gerador;
+      this.cardList[2].consumidor = consumidor;
+    }
+  }
+
+  getGrupo() {
+    let grupoA: number = 0;
+    let grupoB: number = 0;
+    if (this.response) {
+      this.response.map((uc: any) => {
+        uc.Grupo === 'A1' || uc.Grupo === 'A2' || uc.Grupo === 'A3'
+          ? (grupoA = grupoA + 1)
+          : (grupoB = grupoB + 1);
+      });
+      this.cardList[3].grupoB = grupoB;
+      this.cardList[3].grupoA = grupoA;
+    }
+  }
+
+  getAddressAndCpfCount() {
+    let withAddress: number = 0;
+    let withoutAddress: number = 0;
+    let withCPF: number = 0;
+    let withoutCPF: number = 0;
+
+    if (this.response) {
+      this.response.map((uc: any) => {
+        uc.UF
+          ? (withAddress = withAddress + 1)
+          : (withoutAddress = withoutAddress + 1);
+        uc['CPF/CNPJ']
+          ? (withCPF = withCPF + 1)
+          : (withoutCPF = withoutCPF + 1);
+      });
+
+      this.cardList[4].comEndereco = withAddress;
+      this.cardList[4].semEndereco = withoutAddress;
+      this.cardList[4].comCPF = withCPF;
+      this.cardList[4].semCPF = withoutCPF;
+    }
   }
 }
--- src/app/features/pages/uc-register-page/uc-register-page.component.ts
+++ src/app/features/pages/uc-register-page/uc-register-page.component.ts
@@ -1,13 +1,73 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { ToastrService } from 'ngx-toastr';
+import responses from '../../../../../responses-form.json';
 import { ButtonComponent } from '../../components/button/button.component';
+import { InputComponent } from '../../components/input/input.component';
 
 @Component({
   selector: 'app-uc-register-page',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [
+    ButtonComponent,
+    InputComponent,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    CommonModule,
+  ],
+  providers: [
+    provideNgxMask({
+    }),
+  ],
   templateUrl: './uc-register-page.component.html',
-  styleUrl: './uc-register-page.component.scss'
+  styleUrl: './uc-register-page.component.scss',
 })
-export class UcRegisterPageComponent {
+export class UcRegisterPageComponent implements OnInit {
+  private _route = inject(Router);
 
+  private formBuilderService = inject(FormBuilder);
+
+  toast = inject(ToastrService)
+
+  concessionariaList = responses.concessionaria;
+  ufList = responses.UF;
+  tipoList = responses.Tipo;
+  modalidadeList = responses.Modalidade;
+  clienteRuralList = responses['Cliente rural'];
+  organizacaoList = responses['Tipo de organização'];
+  ligacaoList = responses['Tipo de ligação'];
+
+  ngOnInit(): void {
+    // console.log(responses)
+  }
+
+  protected UCForm = this.formBuilderService.group({
+    vigenciaInicial: ['', Validators.required],
+    concessionaria: ['', Validators.required],
+    UF: ['', Validators.required],
+    unidade: ['', Validators.required],
+    cliente: ['', Validators.required],
+    empresa: ['', Validators.required],
+    descricaoUnidade: ['', Validators.required],
+    tipo: ['', Validators.required],
+    modalidade: ['', Validators.required],
+    clienteRural: ['', Validators.required],
+    organizacao: ['', Validators.required],
+    ligacao: ['', Validators.required],
+    cpfOuCnpj: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    senha: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    console.log(this.UCForm.value);
+    this.toast.success('UC cadastrada!', 'Successo!')
+  }
+
+  navigateTo(route: string) {
+    this._route.navigate([route]);
+  }
 }
